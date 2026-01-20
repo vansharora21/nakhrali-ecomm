6488bb48fd7d34c863661137bee4adca779701b4
vansh arora <vansharora4903@gmail.com>
2026-01-20 20:58:12 +0530

    new changes with crud

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -23,8 +23,8 @@ router.post("/", verifyToken, async (req, res) => {
 // UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const updatedCart = await Cart.findByIdAndUpdate(
-      req.params.id,
+    const updatedCart = await Cart.findOneAndUpdate(
+      { userId: req.params.id },
       { $set: req.body },
       { new: true }
     );
@@ -37,7 +37,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 // DELETE
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    await Cart.findByIdAndDelete(req.params.id);
+    await Cart.findOneAndDelete({ userId: req.params.id });
     res.status(200).json("Cart has been deleted...");
   } catch (err) {
     res.status(500).json(err);
